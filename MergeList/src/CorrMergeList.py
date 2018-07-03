5c9d25b4c4ad304197760640c525d266fe021996
tdba <tanguy.debels@gmail.com>
2018-07-03 10:44:00 +0200

    Add editions to respect task order

--- MergeList/src/CorrMergeList.py
+++ MergeList/src/CorrMergeList.py
@@ -2,18 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def merge(first_list, second_list):
-    list = []
-    i,j = 0,0
-    max1,max2 = len(first_list),len(second_list)
-    while (i< max1 and j < max2):
-        if (first_list[i][1] < second_list[j][1]):
-            list.append(first_list[i])
-            i+=1
+    li = []
+    i, j = 0, 0
+    while i < len(first_list) and j < len(second_list):
+        if first_list[i][1] < second_list[j][1]:
+            li.append(first_list[i])
+            i += 1
         else:
-            list.append(second_list[j])
-            j+=1
-    if i< max1:
-        list.extend(first_list[i:])
-    if j< max2:
-        list.extend(second_list[j:])
-    return list   
+            li.append(second_list[j])
+            j += 1
+    if i < len(first_list):
+        li.extend(first_list[i:])
+    if j < len(second_list):
+        li.extend(second_list[j:])
+    return li
